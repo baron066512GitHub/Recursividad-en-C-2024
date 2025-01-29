a25a398934993b1bb78af4cb59efcbcd2103b3bd
baron066512GitHub <isaacbaron90@gmail.com>
2025-01-28 20:36:44 -0400

    Abrir_Archivo.c

--- Abrir_Archivo.c
+++ Abrir_Archivo.c
@@ -1,3 +1,40 @@
 #include <stdio.h>
 
 FILE *p;
+
+int main(){
+    p = fopen("entrada.txt", "r");
+    int r;
+
+    if (p == NULL) {
+        printf("Error al abrir el archivo\n");
+        return 1;
+    }
+
+    for(int i = 0; i<5; i++){
+        fscanf(p,"%d", &r);
+        printf("%d ",r);
+    }
+  
+  fclose(p);
+
+  
+}
+
+/*
+#include <stdio.h>
+
+int main() {
+    FILE *archivo = fopen("datos.txt", "w");
+    int edad = 30;
+    double altura = 1.75;
+    char nombre[] = "Juan";
+
+    fprintf(archivo, "Nombre: %s, Edad: %d, Altura: %.2f\n", nombre, edad, altura);
+
+    fclose(archivo);
+    return 0;
+}
+
+
+*/
